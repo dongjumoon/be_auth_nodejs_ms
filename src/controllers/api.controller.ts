b01e2acfb5f750b75393cf79999de91748b790f5
jinu <jinu9843@naver.com>
2021-05-11 22:48:22 +0900

    지누 커밋

--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 class ApiController {
   public api = (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.status(200).json({ data: 'test', message: 'dongju' });
+      res.status(200).json({ data: 'test', message: 'jinu' });
     } catch (error) {
       next(error);
     }
