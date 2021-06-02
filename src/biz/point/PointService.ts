6f4250e3658e85c36c6f7b033dca984c97a1b6c1
kimyongyeon <toyongyeon@gmail.com>
2021-06-02 21:01:03 +0900

    상품/포인트/주문 로그 추가

--- src/biz/point/PointService.ts
+++ src/biz/point/PointService.ts
@@ -1,3 +1,4 @@
+import { logger } from '@/common/utils/logger';
 import PointException from '@/exceptions/PointException';
 import { PointDTO } from './PointDTO';
 import PointRepository from './PointRepository';
@@ -5,10 +6,13 @@ import PointRepository from './PointRepository';
 export default class PointService {
   private pointRepository = PointRepository;
   public findByUserIdPoint = async (userId: string) => {
-    return await this.pointRepository.findOne({ userId: userId });
+    logger.info(`PointService::findByUserIdPoint in => ${userId}`);
+    const result = await this.pointRepository.findOne({ userId: userId });
+    logger.info(`PointService::findByUserIdPoint out => ${result}`);
   };
 
   public addUserIdPoint = async (pointDTO: PointDTO) => {
+    logger.info(`PointService::addUserIdPoint in => ${JSON.stringify(pointDTO)}`);
     // 기존 포인트 조회
     const updatePointUser = await this.pointRepository.findOne({ userId: pointDTO.userId });
 
@@ -18,15 +22,20 @@ export default class PointService {
       const addScore = updatePointUser.score + 30;
       updatePointUser.score = addScore;
       // 포인트 업데이트
-      return await this.pointRepository.update({ userId: pointDTO.userId }, updatePointUser);
+      const result = await this.pointRepository.update({ userId: pointDTO.userId }, updatePointUser);
+      logger.info(`PointService::addUserIdPoint out => ${result}`);
+      return result;
     } else {
       // 신규
       pointDTO.score = 30;
-      return await this.pointRepository.create(pointDTO);
+      const result = await this.pointRepository.create(pointDTO);
+      logger.info(`PointService::addUserIdPoint out => ${result}`);
+      return result;
     }
   };
 
   public useUserIdPoint = async (pointDTO: PointDTO) => {
+    logger.info(`PointService::useUserIdPoint in => ${JSON.stringify(pointDTO)}`);
     // 기존 포인트 조회
     const updatePointUser = await this.pointRepository.findOne({ userId: pointDTO.userId });
 
@@ -39,10 +48,11 @@ export default class PointService {
       }
       updatePointUser.score = addScore;
       // 포인트 업데이트
-      return await this.pointRepository.update({ userId: pointDTO.userId }, updatePointUser);
+      const result = await this.pointRepository.update({ userId: pointDTO.userId }, updatePointUser);
+      logger.info(`PointService::useUserIdPoint out => ${result}`);
+      return result;
     } else {
       throw new PointException(401, '유저 포인트 사용 불가이며, 신규가입자는 포인트가 없습니다.');
-      return 0;
     }
   };
 }
