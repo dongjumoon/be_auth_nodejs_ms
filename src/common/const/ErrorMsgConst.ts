71dff5d692e4eb876708b85ee4e315b34f33ea36
kimyongyeon <toyongyeon@gmail.com>
2021-06-05 13:51:18 +0900

    메세지 정의 수정

--- src/common/const/ErrorMsgConst.ts
+++ src/common/const/ErrorMsgConst.ts
@@ -3,6 +3,25 @@ export class ErrorMsgConst {
   static readonly ORDER_001_MSG = '데이터 조회가 실패했습니다.';
   static readonly ORDER_001_CODE = 'ORDER_ERROR_001';
 
+  static readonly ORDER_ERROR = [
+    {CODE: 1, MSG: '메세지'},
+    {CODE: 2, MSG: '메세지'},
+    {CODE: 3, MSG: '메세지'},
+    {CODE: 4, MSG: '메세지'}
+  ]
+
+  constructor() {
+    ErrorMsgConst.ORDER_ERROR[0]['CODE']
+    ErrorMsgConst.ORDER_ERROR[0]['MSG']
+
+    ErrorMsgConst.ORDER_ERROR[1]['CODE']
+    ErrorMsgConst.ORDER_ERROR[1]['MSG']
+    
+    ErrorMsgConst.ORDER_ERROR[2]['CODE']
+    ErrorMsgConst.ORDER_ERROR[2]['MSG']
+  }
+
+
   static readonly PRODUCT_001 = '상품에러';
   
   static readonly POINT_001 = '포인트에러';
