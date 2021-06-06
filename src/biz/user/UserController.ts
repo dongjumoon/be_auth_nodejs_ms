eec01b0d668683c72ea3bb65a6faab50f41817fe
DESKTOP-5SVBQ10\jinu9 <11>
2021-06-06 17:53:06 +0900

    user 완료

--- src/biz/user/UserController.ts
+++ src/biz/user/UserController.ts
@@ -1,47 +1,37 @@
-import { NextFunction, Request, Response } from 'express';
-import userService from './UserService';
-import { User } from './UserEntity';
-import { CreateUserDto } from './UserDTO';
-import bcrypt from 'bcrypt';
-import { ErrorDTO, ResponseDTO } from '@/common/dto/ResponseDTO';
 import { ErrorMsgConst } from '@/common/const/ErrorMsgConst';
 import { ResponseMsgConst } from '@/common/const/ResponseMsgConst';
+import { ErrorDTO, ResponseDTO } from '@/common/dto/ResponseDTO';
 import getSeqAutoincrement from '@/common/helper/getSeqAutoincrement';
-import UserRepository from './UserRepository';
-
+import bcrypt from 'bcrypt';
+import { NextFunction, Request, Response } from 'express';
+import { CreateUserDto } from './UserDTO';
+import { User } from './UserEntity';
+import userService from './UserService';
+import _ from 'lodash';
+import { UpdateWriteOpResult } from 'mongoose';
 
 class UsersController {
   public userService = new userService();
-
   public getUsers = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const result: User[] = await this.userService.findAllUser();
-      let response = new ResponseDTO();
-      let errorDTO = new ErrorDTO();
-      if (!result) { // 비정상 조회 일때
-        // 응답코드 
-        response.code = ResponseMsgConst.USER01_400_CODE; // 200, 400, 500...
-        response.msg = ResponseMsgConst.USER01_400_MSG;
-        response.error = errorDTO;
-
-        // 추적하는 유니크 아이디 = ObjectId
-        response.transId = getSeqAutoincrement('getUsers');
-        res.status(200).json(response);
-
-      } else { // 정상일때
-
-        response.code = "200";
-        response.msg = "정상 조회 되었습니다.";
-
-        errorDTO.code = "";
-        errorDTO.msg = "";
-        response.error = errorDTO;
-
-        response.transId = result.map(transId => transId._id).join(";");
-        //response.transId = result._id;
-        response.body = result;
+      const result: {} | User[] = await this.userService.findAllUser();
+      //service 처리 결과 실패 시 성공 시
+      if (_.isEmpty(result)) { // 실패(lodash사용)
+        // 응답코드
+        const response = ResponseDTO.errorProc({
+          title: 'getUsers',
+          error: {
+            code: ErrorMsgConst.USER_ERROR.RL_1.CODE, // 200, 400, 500...
+            msg: ErrorMsgConst.USER_ERROR.RL_1.MSG,
+          },
+          result: result,
+        });
+        res.status(500).json(response);
+      } else { //성공
+        const response = ResponseDTO.successProc(result);
         res.status(200).json(response);
       }
+
     } catch (e) {
       next(e);
     }
@@ -49,37 +39,21 @@ class UsersController {
 
   public getUserById = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const userId: string = req.params.id;
-      const result: User = await this.userService.findUserById(userId);
-      let response = new ResponseDTO();
-      let errorDTO = new ErrorDTO();
-      if (!result) {//비정상 조회 일때
-        //응답코드
-        response.code = ResponseMsgConst.USER01_400_CODE;
-        response.msg = ResponseMsgConst.USER01_400_MSG;
-
-        //에러코드
-        errorDTO.code = ErrorMsgConst.USER_ERROR.RD_4.CODE // 프로젝트에서 정한 에러코드 
-        errorDTO.msg = ErrorMsgConst.USER_ERROR.RD_4.MSG; // 프로젝트에서 정한 에러메세지 
-        response.error = errorDTO;
-
-        // 추적하는 유니크 아이디 = ObjectId
-        response.transId = getSeqAutoincrement('getUserById');
+      const result:  {} | User = await this.userService.findUserById(req.params.id);
+      //service 처리 결과 실패 시 성공 시
+      if (_.isEmpty(result)){ //실패(lodash사용)
+        const response = ResponseDTO.errorProc({
+          title: 'getUserById',
+          error: {
+            code: ErrorMsgConst.USER_ERROR.RD_4.CODE,
+            msg: ErrorMsgConst.USER_ERROR.RD_4.MSG,
+          },
+          result: result,
+        });
+        res.status(500).json(response);
+      } else { //성공
+        const response = ResponseDTO.successProc(result);
         res.status(200).json(response);
-
-      } else { // 정상일때
-
-        response.code = "200";
-        response.msg = "정상 조회 되었습니다.";
-
-        errorDTO.code = "";
-        errorDTO.msg = "";
-        response.error = errorDTO;
-
-        response.transId = result._id;
-        response.body = result;
-        res.status(200).json(response);
-
       }
     } catch (e) {
       next(e);
@@ -89,36 +63,21 @@ class UsersController {
   public deleteUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userId: string = req.params.id;
-      const result: User = await this.userService.deleteUser(userId);
-      let response = new ResponseDTO();
-      let errorDTO = new ErrorDTO();
-      if (!result) {//비정상 조회 일때
-        //응답코드
-        response.code = ResponseMsgConst.USER01_400_CODE;
-        response.msg = ResponseMsgConst.USER01_400_MSG;
+      const result:  {} | User = await this.userService.deleteUser(userId);
+      if (_.isEmpty(result)){
+        const response = ResponseDTO.errorProc({
+          title: 'deleteUser',
+          error: {
+            code: ErrorMsgConst.USER_ERROR.D0_5.CODE,
+            msg: ErrorMsgConst.USER_ERROR.D0_5.MSG,
+          },
+          result: result,
+        });
+        res.status(500).json(response);
 
-        //에러코드
-        errorDTO.code = ErrorMsgConst.USER_ERROR.D0_5.CODE // 프로젝트에서 정한 에러코드 
-        errorDTO.msg = ErrorMsgConst.USER_ERROR.D0_5.MSG; // 프로젝트에서 정한 에러메세지 
-        response.error = errorDTO;
-
-        // 추적하는 유니크 아이디 = ObjectId
-        response.transId = getSeqAutoincrement('getUserById');
+      } else {
+        const response = ResponseDTO.successProc(result);
         res.status(200).json(response);
-
-      } else { // 정상일때
-
-        response.code = "200";
-        response.msg = "정상 삭제 되었습니다.";
-
-        errorDTO.code = "";
-        errorDTO.msg = "";
-        response.error = errorDTO;
-
-        response.transId = result._id;
-        response.body = result;
-        res.status(200).json(response);
-
       }
     } catch (e) {
       next(e);
@@ -127,35 +86,20 @@ class UsersController {
 
   public createUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const result: User = await this.userService.createUser(req.body);
-      let response = new ResponseDTO();
-      let errorDTO = new ErrorDTO();
-      if (!result) {//비정상 조회 일때
-        //응답코드
-        response.code = ResponseMsgConst.USER01_400_CODE;
-        response.msg = ResponseMsgConst.USER01_400_MSG;
-
-        //에러코드
-        errorDTO.code = ErrorMsgConst.USER_ERROR.C0_2.CODE // 프로젝트에서 정한 에러코드 
-        errorDTO.msg = ErrorMsgConst.USER_ERROR.C0_2.MSG; // 프로젝트에서 정한 에러메세지 
-        response.error = errorDTO;
-
-        // 추적하는 유니크 아이디 = ObjectId
-        response.transId = getSeqAutoincrement('createUser');
-        res.status(200).json(response);
+      const result: {} | User = await this.userService.createUser(req.body);
+      if (_.isEmpty(result)){ //실패(lodash사용)
+        const response = ResponseDTO.errorProc({
+          title: 'createUser',
+          error: {
+            code: ErrorMsgConst.USER_ERROR.C0_2.CODE,
+            msg: ErrorMsgConst.USER_ERROR.C0_2.MSG,
+          },
+          result: result,
+        });
+        res.status(500).json(response);
       } else { // 정상일때
-
-        response.code = "200";
-        response.msg = "정상 등록 되었습니다.";
-
-        errorDTO.code = "";
-        errorDTO.msg = "";
-        response.error = errorDTO;
-
-        response.transId = result._id;
-        response.body = result;
+        const response = ResponseDTO.successProc(result);
         res.status(200).json(response);
-
       }
     } catch (e) {
       next(e);
@@ -164,36 +108,26 @@ class UsersController {
 
   public updateUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const userId: string = req.body.user_id;
-      const userData: CreateUserDto = {
-        password: String(req.body.password),
-      };
+      //const userId: string = req.body.user_id;
+      // const userData: CreateUserDto = {
+      //   password: String(req.body.password),
+      // };
       const hashedPassword = await bcrypt.hash(req.body.password, 10);
       req.body.password = hashedPassword;
-      const result: User = await this.userService.updateUser(userId, req.body);
-      let response = new ResponseDTO();
-      let errorDTO = new ErrorDTO();
-      if (!result) { // 비정상 조회 일때
-        response.code = ResponseMsgConst.ORDER_400_CODE; // 200, 400, 500...
-        response.msg = ResponseMsgConst.ORDER_400_MSG;
-
-        //에러코드
-        errorDTO.code = ErrorMsgConst.USER_ERROR.U0_3.CODE // 프로젝트에서 정한 에러코드 
-        errorDTO.msg = ErrorMsgConst.USER_ERROR.U0_3.MSG; // 프로젝트에서 정한 에러메세지 
-        response.error = errorDTO;
-
-      } else { // 정상일때
-        response.code = "200";
-        response.msg = "정상 등록 되었습니다.";
-
-        errorDTO.code = "";
-        errorDTO.msg = "";
-        response.error = errorDTO;
-
-        response.transId = result._id;
-        response.body = result;
+      const result:  User | {} = await this.userService.updateUser(req.body);
+      if (_.isEmpty(result)){
+        const response = ResponseDTO.errorProc({
+          title: 'updateUser',
+          error: {
+            code: ErrorMsgConst.USER_ERROR.U0_3.CODE,
+            msg: ErrorMsgConst.USER_ERROR.U0_3.MSG,
+          },
+          result: result,
+        });
+        res.status(500).json(response);
+      } else {
+        const response = ResponseDTO.successProc(result);
         res.status(200).json(response);
-
       }
     } catch (e) {
       next(e);
