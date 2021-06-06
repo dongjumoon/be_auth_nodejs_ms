72ba2834512deb401d03d85d9fc8b1aaf19297e2
kimyongyeon <toyongyeon@gmail.com>
2021-06-07 08:24:43 +0900

    배너 서비스 만드중

--- src/biz/banner/BannerRepository.ts
+++ src/biz/banner/BannerRepository.ts
@@ -2,10 +2,46 @@ import { model, Schema, Document } from 'mongoose';
 import { BannerEntity } from './BannerEntity';
 
 const BannerSchema: Schema = new Schema({
-  userId: {
+  bannerId: {
     type: String,
     required: true,
     unique: true,
+  },
+  name: {
+    type: String,
+  },
+  imgUrl: {
+    type: String,
+  },
+  width: {
+    type: Number,
+  },
+  height: {
+    type: Number,
+  },
+  location: { // top, mid, bot
+    type: String,
+  },
+  useYn: { // Y, N
+    type: String,
+  },
+  startDate: { // 20210607010000
+    type: String,
+  },
+  endDate: { // 20210607010000
+    type: String,
+  },
+  title: { 
+    type: String,
+  },
+  description: { 
+    type: String,
+  },
+  sort: { 
+    type: Number,
+  },
+  deployYn: { 
+    type: String,
   },
 });
 
