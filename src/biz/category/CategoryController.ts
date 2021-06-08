f52e63d4944c31ecea6faf84082acac63f42d5d1
p151541 <jobkim0410@gmail.com>
2021-06-08 13:15:27 +0900

    카테고리 기능 추가

---
+++ src/biz/category/CategoryController.ts
@@ -0,0 +1,108 @@
+import { ResponseDTO } from "@/common/dto/ResponseDTO";
+import { NextFunction, Request, Response } from "express";
+import { CategoryDTO } from "./CategoryDTO";
+import CategoryRepository from "./CategoryRepository";
+import CategoryService from "./CategoryService";
+
+export default class CategoryController {
+  public categoryService: CategoryService;
+  constructor(categoryService: CategoryService) {
+    this.categoryService = categoryService;
+  }
+  /**
+   * 카테고리 타입별 조회 
+   * @param req 
+   * @param res 
+   * @param next 
+   */
+  public listByType = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const type = req.params.type;
+      const result = this.categoryService.listByType(type);
+      const response = ResponseDTO.successProc(result);
+      res.status(200).json(response);
+    } catch(e) {
+      next(e);
+    }
+  }
+  /**
+   * 카테고리 상세조회 
+   * @param req 
+   * @param res 
+   * @param next 
+   */
+  public detailByCode = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const code = req.params.code;
+      const result = this.categoryService.detailByCode(code);
+      const response = ResponseDTO.successProc(result);
+      res.status(200).json(response);
+    } catch(e) {
+      next(e);
+    }
+  }
+  /**
+   * 카테고리 등록 
+   * @param req 
+   * @param res 
+   * @param next 
+   */
+  public reg = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const categoryDTO = req.body as unknown as CategoryDTO;
+      const result = this.categoryService.reg(categoryDTO);
+      const response = ResponseDTO.successProc(result);
+      res.status(200).json(response);
+    } catch(e) {
+      next(e);
+    }
+  }
+  /**
+   * 카테고리 수정
+   * @param req 
+   * @param res 
+   * @param next 
+   */
+  public edit = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const categoryDTO = req.body as unknown as CategoryDTO;
+      const result = this.categoryService.edit(categoryDTO);
+      const response = ResponseDTO.successProc(result);
+      res.status(200).json(response);
+    } catch(e) {
+      next(e);
+    }
+  }
+  /**
+   * 카테고리 논리삭제 
+   * @param req 
+   * @param res 
+   * @param next 
+   */
+  public remove = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const categoryDTO = req.body as unknown as CategoryDTO;
+      const result = this.categoryService.remove(categoryDTO);
+      const response = ResponseDTO.successProc(result);
+      res.status(200).json(response);
+    } catch(e) {
+      next(e);
+    }
+  }
+  /**
+   * 카테고리 물리삭제 
+   * @param req 
+   * @param res 
+   * @param next 
+   */
+  public delete = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const categoryDTO = req.body as unknown as CategoryDTO;
+      const result = this.categoryService.delete(categoryDTO);
+      const response = ResponseDTO.successProc(result);
+      res.status(200).json(response);
+    } catch(e) {
+      next(e);
+    }
+  }
+}
