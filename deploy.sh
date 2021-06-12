33d628175432ebc0657c00c90418e9efb15dd5b5
Cloud User <centos@ip-172-26-0-84.ap-northeast-2.compute.internal>
2021-06-13 01:29:55 +0900

    deploy.sh 수정중

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash 
 sudo docker build . -f Dockerfile-Dev -t toyongyeon/be_auth_nodejs_ms_dev:0.1
-sudo docker run --name be-auth-nodejs-ms-dev -e MONGO_HOST=$MONGO_HOST -d -p 3000:8888 toyongyeon/be_auth_nodejs_ms_dev:0.1
+# sudo docker run --name be-auth-nodejs-ms-dev -e MONGO_HOST=$MONGO_HOST -d -p 3000:8888 toyongyeon/be_auth_nodejs_ms_dev:0.1
+sudo docker restart be-auth-nodejs-ms-dev
