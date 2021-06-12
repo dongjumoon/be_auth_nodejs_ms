5b9fc0c5ea57df3b5f8dd13a8b8d1a257b915145
kimyongyeon <toyongyeon@gmail.com>
2021-06-12 22:01:29 +0900

    options 200 add

--- src/app.ts
+++ src/app.ts
@@ -62,9 +62,13 @@ class App {
     //   // this.app.use(cors({ origin: 'coffee-front.co.kr', credentials: true }));
     //   // this.app.use(cors());
     // }
-    this.app.use (cors ({ 
-      credentials : true, 
-    }));
+
+    var corsOptions = {
+      origin: 'http://front.coffee.co.kr',
+      optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    }
+
+    this.app.use (cors (corsOptions));
     this.app.use(hpp());
     this.app.use(compression());
     this.app.use(express.json());
